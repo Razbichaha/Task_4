0fd0a5641dbc1998ed8e94bdc7ef4fa42031c99c
Сергей <seregabur@live.ru>
2022-03-23 21:14:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вас зовут Алексей, вам 21 год, вы водолей и работаете на заводе.
+
+            
+
+            string[] age = { "Привет \nСейчас вам будет заданно несколько вопросов. \nПосле ввода ответа нажмите клавишу Enter. ", "Вас зовут", "Сколько вам лет", "Каков ваш знак зодиака", "Где вы работаете" };
+            string[] templateAnswer = { "Вас зовут", "Ваш возраст", "Вы родились под знаком зодиака", "Вы работаете" };
+            //string work = "Где вы работаете";
+            string mark = " ?";
+         
+            string answer = "";
+            for (int i=0;i<=age.Length-1;i++)
+            {
+                if (i != 0)
+                {
+                    Console.WriteLine(age[i]+mark);
+                    // Console.ReadLine();
+                    answer = answer + templateAnswer[i-1]+" - " + Console.ReadLine()+". ";
+                answer = answer + " ";
+                }
+                else
+                {
+                Console.WriteLine(age[i]);
+                Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
